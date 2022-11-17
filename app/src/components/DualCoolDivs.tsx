c4ae4dee0bd45ff86825da893fcc08e7d7a93690
David Hérault <dherault@gmail.com>
2022-11-17 03:39:12 +0100

    [ecu] Create component DualCoolDivs

---
+++ app/src/components/DualCoolDivs.tsx
@@ -0,0 +1,26 @@
+/* --
+* IMPORTS START
+-- */
+import { Div } from 'ecu'
+
+/* --
+* IMPORTS END
+-- */
+/* --
+* TYPES START
+-- */
+type DualCoolDivsPropsType = Record<string, never>;
+
+/* --
+* TYPES END
+-- */
+
+function DualCoolDivs(props: DualCoolDivsPropsType) {
+  return (
+    <Div data-ecu="h53m0nrgvj:0">
+      Edit me I'm famous!
+    </Div>
+  )
+}
+
+export default DualCoolDivs
