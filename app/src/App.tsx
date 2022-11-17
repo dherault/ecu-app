b1607def3d32c8f3a7080e502fe22692fce5dc43
David Hérault <dherault@gmail.com>
2022-11-17 17:08:25 +0100

    [ecu] Edit text on component App

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -29,7 +29,7 @@ function App(props: AppPropsType) {
       <CoolDiv />
       <CoolDiv />
       
-  <Text data-ecu="liPz3SHBxY:1">App!</Text>
+  <Text data-ecu="liPz3SHBxY:1">App!!</Text>
       <DualCoolDivs />
     </>
   )
