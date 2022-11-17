490ff9c509ff36b442400bf2cae081328dd6d63c
David Hérault <dherault@gmail.com>
2022-11-17 03:40:58 +0100

    [ecu] Create component Test2

---
+++ app/src/components/Test2.tsx
@@ -0,0 +1,26 @@
+/* --
+* IMPORTS START
+-- */
+import { Div } from 'ecu'
+
+/* --
+* IMPORTS END
+-- */
+/* --
+* TYPES START
+-- */
+type Test2PropsType = Record<string, never>;
+
+/* --
+* TYPES END
+-- */
+
+function Test2(props: Test2PropsType) {
+  return (
+    <Div data-ecu="99hbkNIdIZ:0">
+      Edit me I'm famous!
+    </Div>
+  )
+}
+
+export default Test2
