84ec7c6143ef07f156116b64944764d7d1ae641c
David Hérault <dherault@gmail.com>
2022-11-21 23:31:43 +0100

    [ecu] Edit text on component App

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -44,7 +44,7 @@ function App(props: AppPropsType) {
       </Div>
       <Text data-ecu="dZL1Xh1Cuv:1">:)</Text>
       <Text data-ecu="dZL1Xh1Cuv:2">{'1\n2'}</Text>
-      <Text data-ecu="dZL1Xh1Cuv:3">1</Text>
+      <Text data-ecu="dZL1Xh1Cuv:3" />
       <Text data-ecu="dZL1Xh1Cuv:4">First double click here</Text>
       <CoolDiv />
       <Text data-ecu="dZL1Xh1Cuv:5">Then double click here!</Text>
