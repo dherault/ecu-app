f3d0f5a86918403821224fcbd5e1ab1abd727a93
David Hérault <dherault@gmail.com>
2022-11-22 22:27:00 +0100

    [ecu] Edit hierarchy display name on App

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -54,7 +54,8 @@ function App(props: AppPropsType) {
       </Text>
       <CoolDiv // ecu-display-name Cool
       />
-      <CoolDiv />
+      <CoolDiv // ecu-display-name Cool2
+      />
       <Text data-ecu="la53NIPkq6:2">{'1\n2\n3\n4'}</Text>
       <Text data-ecu="la53NIPkq6:3">Foo!</Text>
       <Text data-ecu="la53NIPkq6:4">First double click here</Text>
