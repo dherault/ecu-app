1f36d4385dd7100b4fd9b49dab209ba470787211
David Hérault <dherault@gmail.com>
2022-11-22 23:32:54 +0100

    [ecu] Edit hierarchy display name on App

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -39,8 +39,7 @@ The app, splendid!!!
 function App(props: AppPropsType) {
   return (
     <>
-      <Div // ecu-display-name Div
-        data-ecu="la53NIPkq6:0">
+      <Div data-ecu="la53NIPkq6:0">
         <Text data-ecu="la53NIPkq6:0_0">
           :D :D :D
         </Text>
