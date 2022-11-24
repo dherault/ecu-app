b7174f55db0b2b9cb0ab69da9ac8d301f840db22
David Hérault <dherault@gmail.com>
2022-11-24 03:30:48 +0100

    [ecu] Add component CoolDiv to App

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -54,6 +54,7 @@ function App(props: AppPropsType) {
       <CoolDiv />
       <CoolDiv />
       <CoolDiv />
+      <CoolDiv />
       <Text data-ecu="la53NIPkq6:2">
         {'1\n2\n3\n4'}
       </Text>
