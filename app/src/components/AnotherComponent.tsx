11f1b8151bdbbd9becb30bacb537a258659aba3a
David Hérault <dherault@gmail.com>
2022-11-25 04:08:37 +0100

    [ecu] Create component AnotherComponent

---
+++ app/src/components/AnotherComponent.tsx
@@ -0,0 +1,38 @@
+/* --
+* IMPORTS START
+-- */
+import { Text } from 'ecu'
+
+/* --
+* IMPORTS END
+-- */
+/* --
+* TYPES START
+-- */
+type AnotherComponentPropsType = Record<string, never>;
+
+/* --
+* TYPES END
+-- */
+/* --
+  * EMOJI START
+-- */
+/* --
+  * EMOJI END
+-- */
+/* --
+  * DESCRIPTION START
+-- */
+/* --
+  * DESCRIPTION END
+-- */
+
+function AnotherComponent(props: AnotherComponentPropsType) {
+  return (
+    <Text data-ecu="yA5D_3_6bm:0">
+      Edit me I'm famous!
+    </Text>
+  )
+}
+
+export default AnotherComponent
