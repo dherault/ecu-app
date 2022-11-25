cf82d6acd9e7bfdf4de9bf495fcabf1b07e74d9b
David Hérault <dherault@gmail.com>
2022-11-25 04:04:57 +0100

    [ecu] Create component TripleCoolDiv

---
+++ app/src/components/TripleCoolDiv.tsx
@@ -0,0 +1,38 @@
+/* --
+* IMPORTS START
+-- */
+import { Text } from 'ecu'
+
+/* --
+* IMPORTS END
+-- */
+/* --
+* TYPES START
+-- */
+type TripleCoolDivPropsType = Record<string, never>;
+
+/* --
+* TYPES END
+-- */
+/* --
+  * EMOJI START
+-- */
+/* --
+  * EMOJI END
+-- */
+/* --
+  * DESCRIPTION START
+-- */
+/* --
+  * DESCRIPTION END
+-- */
+
+function TripleCoolDiv(props: TripleCoolDivPropsType) {
+  return (
+    <Text data-ecu="N74MjuS1cn:0">
+      Edit me I'm famous!
+    </Text>
+  )
+}
+
+export default TripleCoolDiv
