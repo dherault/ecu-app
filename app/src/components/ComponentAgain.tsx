34d704cef39ac99b0bd933b27b67aef211231a28
David Hérault <dherault@gmail.com>
2022-11-25 04:13:49 +0100

    [ecu] Create component ComponentAgain

---
+++ app/src/components/ComponentAgain.tsx
@@ -0,0 +1,38 @@
+/* --
+* IMPORTS START
+-- */
+import { Text } from 'ecu'
+
+/* --
+* IMPORTS END
+-- */
+/* --
+* TYPES START
+-- */
+type ComponentAgainPropsType = Record<string, never>;
+
+/* --
+* TYPES END
+-- */
+/* --
+  * EMOJI START
+-- */
+/* --
+  * EMOJI END
+-- */
+/* --
+  * DESCRIPTION START
+-- */
+/* --
+  * DESCRIPTION END
+-- */
+
+function ComponentAgain(props: ComponentAgainPropsType) {
+  return (
+    <Text data-ecu="ut38Vd5UVv:0">
+      Edit me I'm famous!
+    </Text>
+  )
+}
+
+export default ComponentAgain
