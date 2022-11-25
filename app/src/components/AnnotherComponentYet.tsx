22be4f9978d9ec4952464311827fbe620174422a
David Hérault <dherault@gmail.com>
2022-11-25 04:09:20 +0100

    [ecu] Create component AnnotherComponentYet

---
+++ app/src/components/AnnotherComponentYet.tsx
@@ -0,0 +1,38 @@
+/* --
+* IMPORTS START
+-- */
+import { Text } from 'ecu'
+
+/* --
+* IMPORTS END
+-- */
+/* --
+* TYPES START
+-- */
+type AnnotherComponentYetPropsType = Record<string, never>;
+
+/* --
+* TYPES END
+-- */
+/* --
+  * EMOJI START
+-- */
+/* --
+  * EMOJI END
+-- */
+/* --
+  * DESCRIPTION START
+-- */
+/* --
+  * DESCRIPTION END
+-- */
+
+function AnnotherComponentYet(props: AnnotherComponentYetPropsType) {
+  return (
+    <Text data-ecu="qExqGbocz5:0">
+      Edit me I'm famous!
+    </Text>
+  )
+}
+
+export default AnnotherComponentYet
