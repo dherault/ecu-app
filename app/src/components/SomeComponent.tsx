01097e1b80819cf6144ca67afb4ccd753ed29114
David Hérault <dherault@gmail.com>
2022-11-25 04:07:17 +0100

    [ecu] Create component SomeComponent

---
+++ app/src/components/SomeComponent.tsx
@@ -0,0 +1,38 @@
+/* --
+* IMPORTS START
+-- */
+import { Text } from 'ecu'
+
+/* --
+* IMPORTS END
+-- */
+/* --
+* TYPES START
+-- */
+type SomeComponentPropsType = Record<string, never>;
+
+/* --
+* TYPES END
+-- */
+/* --
+  * EMOJI START
+-- */
+/* --
+  * EMOJI END
+-- */
+/* --
+  * DESCRIPTION START
+-- */
+/* --
+  * DESCRIPTION END
+-- */
+
+function SomeComponent(props: SomeComponentPropsType) {
+  return (
+    <Text data-ecu="BBLFObEEmQ:0">
+      Edit me I'm famous!
+    </Text>
+  )
+}
+
+export default SomeComponent
