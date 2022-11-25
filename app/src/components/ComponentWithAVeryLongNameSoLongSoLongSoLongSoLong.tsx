23055e2f424f35ad596ebd3f1712f0f2811a6e53
David Hérault <dherault@gmail.com>
2022-11-25 05:29:37 +0100

    [ecu] Create component ComponentWithAVeryLongNameSoLongSoLongSoLongSoLong

---
+++ app/src/components/ComponentWithAVeryLongNameSoLongSoLongSoLongSoLong.tsx
@@ -0,0 +1,38 @@
+/* --
+* IMPORTS START
+-- */
+import { Text } from 'ecu'
+
+/* --
+* IMPORTS END
+-- */
+/* --
+* TYPES START
+-- */
+type ComponentWithAVeryLongNameSoLongSoLongSoLongSoLongPropsType = Record<string, never>;
+
+/* --
+* TYPES END
+-- */
+/* --
+  * EMOJI START
+-- */
+/* --
+  * EMOJI END
+-- */
+/* --
+  * DESCRIPTION START
+-- */
+/* --
+  * DESCRIPTION END
+-- */
+
+function ComponentWithAVeryLongNameSoLongSoLongSoLongSoLong(props: ComponentWithAVeryLongNameSoLongSoLongSoLongSoLongPropsType) {
+  return (
+    <Text data-ecu="50mT6HUBvT:0">
+      Edit me I'm famous!
+    </Text>
+  )
+}
+
+export default ComponentWithAVeryLongNameSoLongSoLongSoLongSoLong
