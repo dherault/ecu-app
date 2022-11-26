b7bdd63a62fd9de5e5747b9d31e1d3ae743ccf6a
David Hérault <dherault@gmail.com>
2022-11-26 05:04:30 +0100

    Edit Decorator

--- app/src/decorators/EcuCommonDecorator.tsx
+++ app/src/decorators/EcuCommonDecorator.tsx
@@ -2,15 +2,27 @@
  * DO NOT DELETE THIS FILE
  * EcuCommonDecorator.tsx is used to decorate any Ecu component
 -- */
-import '../index.css'
+import { ReactNode, useEffect } from 'react'
 
-import { ReactNode } from 'react'
+import indexCss from '../index.css?inline'
 
 type EcuCommonDecoratorPropsType = {
+  head?: HTMLHeadElement
   children: ReactNode
 }
 
-function EcuCommonDecorator({ children }: EcuCommonDecoratorPropsType) {
+function EcuCommonDecorator({ children, head }: EcuCommonDecoratorPropsType) {
+
+  useEffect(() => {
+    if (!head) return
+
+    const style = document.createElement('style')
+
+    style.innerHTML = indexCss
+
+    head.appendChild(style)
+  }, [head])
+
   return children
 }
 
