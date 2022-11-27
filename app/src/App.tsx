059411a62d7a6805ee2d5e0ff862ddd6637666a8
David Hérault <dherault@gmail.com>
2022-11-27 02:36:16 +0100

    [ecu] Add className 'foo baz bar' to Text in App

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -48,7 +48,7 @@ function App(props: AppPropsType) {
         </Text>
       </Div>
       <Text
-        className="foo baz"
+        className="foo baz bar"
         data-ecu="y7qE5GENbS:1">
         Foo className
       </Text>
