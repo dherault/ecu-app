5c0281ed489f6420237c46bdc9357a3c02786dd5
David Hérault <dherault@gmail.com>
2022-11-27 23:19:28 +0100

    [ecu] Add className 'margin-x padding-y red' to Text in App

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -48,7 +48,7 @@ function App(props: AppPropsType) {
         </Text>
       </Div>
       <Text
-        className="margin-x padding-y"
+        className="margin-x padding-y red"
         data-ecu="B66AQZfdKS:1">
         Red className
       </Text>
