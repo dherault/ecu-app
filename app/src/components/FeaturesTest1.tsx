8fe69db557ef62ca2a1f3fc49e58ee8adaef29ad
David Hérault <dherault@gmail.com>
2022-11-28 04:55:15 +0100

    [ecu] Create component FeaturesTest1

---
+++ app/src/components/FeaturesTest1.tsx
@@ -0,0 +1,38 @@
+/* --
+* IMPORTS START
+-- */
+import { Text } from 'ecu'
+
+/* --
+* IMPORTS END
+-- */
+/* --
+* TYPES START
+-- */
+type FeaturesTest1PropsType = Record<string, never>;
+
+/* --
+* TYPES END
+-- */
+/* --
+  * EMOJI START
+-- */
+/* --
+  * EMOJI END
+-- */
+/* --
+  * DESCRIPTION START
+-- */
+/* --
+  * DESCRIPTION END
+-- */
+
+function FeaturesTest1(props: FeaturesTest1PropsType) {
+  return (
+    <Text data-ecu="l4b0XKVLWM:0">
+      Edit me I'm famous!
+    </Text>
+  )
+}
+
+export default FeaturesTest1
