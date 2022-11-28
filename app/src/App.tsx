99226d04cee8ccae37a7d68c78a0ec165f8c5c0e
David Hérault <dherault@gmail.com>
2022-11-28 01:39:45 +0100

    [ecu] Add className 'margin-x red padding-y flex foo random' to Text in App

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -48,7 +48,7 @@ function App(props: AppPropsType) {
         </Text>
       </Div>
       <Text
-        className="margin-x red padding-y flex foo random padding-top"
+        className="margin-x red padding-y flex foo random"
         data-ecu="JyPBoWBHsp:1">
         Red className
       </Text>
