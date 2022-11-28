76a5483f6c2ffbf8dcaee1ec0394dbb8913002bc
David Hérault <dherault@gmail.com>
2022-11-28 06:19:09 +0100

    [ecu] Delete component Div in FeaturesTest1

--- app/src/components/FeaturesTest1.tsx
+++ app/src/components/FeaturesTest1.tsx
@@ -33,10 +33,10 @@ function FeaturesTest1(props: FeaturesTest1PropsType) {
       <Div
         className="div-remove-me"
         data-ecu="4_PFmJeDFmc:0" />
+      
       <Div data-ecu="4_PFmJeDFmc:1" />
       <Div data-ecu="4_PFmJeDFmc:2" />
-      <Div data-ecu="4_PFmJeDFmc:3" />
-      <Text data-ecu="4_PFmJeDFmc:4">
+      <Text data-ecu="4_PFmJeDFmc:3">
         Edit me I'm famous!
       </Text>
     </>
