8024f6a716a101c3f4197f26556ed7c95f84c82e
David Hérault <dherault@gmail.com>
2022-11-28 15:15:28 +0100

    [ecu] Add className 'margin-x margin-top padding-y flex-reverse hero-❤️' to Text in App

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -48,7 +48,7 @@ function App(props: AppPropsType) {
         </Text>
       </Div>
       <Text // ecu-display-name Red className
-        className="red margin-x margin-top padding-y flex-reverse hero-❤️"
+        className="margin-x margin-top padding-y flex-reverse hero-❤️"
         data-ecu="9j3ghBqjqJE:1">
         Red className
       </Text>
