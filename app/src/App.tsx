1c2da9af23247620b5c5f247bdd2380e06f87be8
David Hérault <dherault@gmail.com>
2022-11-29 04:31:19 +0100

    [ecu] Add className 'another-cool-class yet-another-cool-class again-cool-class' to Text in App

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -63,7 +63,7 @@ function App(props: AppPropsType) {
       <ComponentWithAVeryLongNameSoLongSoLongSoLongSoLong // ecu-display-name LongLongLongLongLongLongLongLong
       />
       <Text
-        className="another-cool-class yet-another-cool-class again-cool-class"
+        className="yet-another-cool-class again-cool-class"
         data-ecu="ZPiWYCu9g:4">
         Responsive
       </Text>
