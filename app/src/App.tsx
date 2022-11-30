66e2bb3847f3debefdfd17e55e47182db043d73b
David Hérault <dherault@gmail.com>
2022-11-30 11:02:30 +0100

    [ecu] Add className 'width cool-class' to Text in App

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -48,7 +48,7 @@ function App(props: AppPropsType) {
         </Text>
       </Div>
       <Text // ecu-display-name Red className
-        className="width"
+        className="width cool-class"
         data-ecu="VuY5TWpk1O6:1">
         Red className
       </Text>
