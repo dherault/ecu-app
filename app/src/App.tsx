4a998bdf9deb8c30ef878023dad846cb3a8aaeb7
David Hérault <dherault@gmail.com>
2022-11-30 14:30:49 +0100

    [ecu] Add className 'max-height foobar' to Text in App

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -56,7 +56,7 @@ function App(props: AppPropsType) {
       <CoolDiv // ecu-display-name Some cool div
       />
       <Text
-        className="max-height"
+        className="max-height foobar"
         data-ecu="VuY5TWpk1O6:2">
         {'1\n2\n3\n4\n5\n6'}
       </Text>
