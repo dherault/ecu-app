ee99d259cc80321a9714493a3a5b9f60426fd160
David Hérault <dherault@gmail.com>
2022-11-30 11:03:29 +0100

    [ecu] Remove className on Text in App

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -48,7 +48,6 @@ function App(props: AppPropsType) {
         </Text>
       </Div>
       <Text // ecu-display-name Red className
-        className="width"
         data-ecu="VuY5TWpk1O6:1">
         Red className
       </Text>
