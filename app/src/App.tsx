9db57bfb9b67093687f2d9ecb6c9ec9f396df1ea
David Hérault <dherault@gmail.com>
2022-12-04 11:41:10 +0100

    [ecu] Add component Text to App

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -49,6 +49,7 @@ function App(props: AppPropsType) {
           data-ecu="-dhHA4b8dY:1_0">
           <Text data-ecu="-dhHA4b8dY:1_0_0">Edit me I'm a Text</Text>
           <Text data-ecu="-dhHA4b8dY:1_0_1">Edit me I'm a Text</Text>
+          <Text data-ecu="-dhHA4b8dY:1_0_2">Edit me I'm a Text</Text>
         </Div>
         <Text // ecu-display-name Text!
           className="width"
