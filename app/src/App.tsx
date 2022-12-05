165f7e7ba7e247c1777ff99760676e9f3eba7ca5
David Hérault <dherault@gmail.com>
2022-12-05 11:13:53 +0100

    [ecu] Edit text on component App

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -58,7 +58,7 @@ function App(props: AppPropsType) {
           data-ecu="ZU2pYPalY:2_0">
           <Text data-ecu="ZU2pYPalY:2_0_0">Text 1</Text>
           <Text data-ecu="ZU2pYPalY:2_0_1">Text 2</Text>
-          <Text data-ecu="ZU2pYPalY:2_0_2">Edit me I'm a Text</Text>
+          <Text data-ecu="ZU2pYPalY:2_0_2">Text 3</Text>
           <Text data-ecu="ZU2pYPalY:2_0_3">Edit me I'm a Text</Text>
         </Div>
         <Text // ecu-display-name Text!
