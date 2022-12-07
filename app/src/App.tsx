b011cf851b1b603d729752d7bab74a7f85615d71
David Hérault <dherault@gmail.com>
2022-12-07 17:52:14 +0100

    [ecu] Add className 'bp-order-6 height-overflow' to Text in App

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -73,7 +73,7 @@ function App(props: AppPropsType) {
         </Text>
       </Div>
       <Text // ecu-display-name Red className
-        className="bp-order-6"
+        className="bp-order-6 height-overflow"
         data-ecu="Ysm52JVO0:3">
         Red className
       </Text>
