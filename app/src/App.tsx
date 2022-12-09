687266cc4e474da0959789196449a35e11c07a6f
David Hérault <dherault@gmail.com>
2022-12-09 07:00:31 +0100

    Prep for more challenges

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,43 +1,79 @@
 // ecu-file-id 8txd0jvsM
-import { useState } from 'react'
+import { useCallback, useEffect, useMemo, useState } from 'react'
 
 import CoolDiv from './components/CoolDiv'
 import CoolDaddy from './components/CoolDaddy'
 
 const onetwothree = [1, 2, 3]
 
+function useCustomHook(n: number) {
+  return useMemo(() => n * n, [n])
+}
+
 // ecu-function-id 0pmQZMibKj
 function App() {
   const [count, setCount] = useState(1)
+
+  const memoedValue = useMemo(() => 123, [])
+  const fromCustomHookValue = useCustomHook(12)
+
+  function logMe() {
+    console.log('I will return')
+
+    return true
+  }
+
+  const renderSomeDiv = useCallback(() => count % 2 === 0 && (
+    <div>I was rendered by a useCallback on even count</div>
+  ), [count])
+
+  const renderSomeDiv2 = useCallback(() => {
+    if (count % 2) return null
+    if (count % 2 === 1) return 'I was rendered by a useCallback on some count'
+
+    return (
+      <div>I was rendered by a useCallback on even count</div>
+    )
+  }, [count])
+
+  useEffect(() => {
+    console.log('I\'m within a useEffect')
+  }, [])
 
   return (
     <>
       <div>A div!</div>
       <p>A p!</p>
+      Some raw text
+      {renderSomeDiv()}
+      {renderSomeDiv2()}
+      <div>
+        Here is a memoed value:
+        {' '}
+        {memoedValue}
+      </div>
+      <div>
+        Here is a value from a custom hook:
+        {' '}
+        {fromCustomHookValue}
+      </div>
       <button
         type="button"
-        onClick={() => setCount(x => x + 1)}>
+        onClick={() => setCount(x => x + 1)}
+      >
         {count}
       </button>
       <button
         type="button"
-        onClick={() => console.log('a log')}>
+        onClick={() => console.log('a log')}
+      >
         A log button!
       </button>
       <button
         type="button"
-        onClick={() => console.warn('a log')}>
-        A warn button!
-      </button>
-      <button
-        type="button"
-        onClick={() => console.error('a log')}>
-        An error button!
-      </button>
-      <button
-        type="button"
-        onClick={() => console.debug('a log')}>
-        A debug button!
+        onClick={logMe}
+      >
+        A function button!
       </button>
       <CoolDiv />
       <CoolDaddy>
