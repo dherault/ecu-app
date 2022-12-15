2ec86cdc7202e70ee56b29743c31242712856871
David Hérault <dherault@gmail.com>
2022-12-15 07:42:50 +0100

    [ecu] Add className cool-div-3 to div

--- app/src/components/CoolDiv.tsx
+++ app/src/components/CoolDiv.tsx
@@ -9,7 +9,7 @@
 // ecu-function-id lj9bmEmMw-
 // ecu-file-id vk2xejunrL
 function CoolDiv() {
-  return <div>I'm a cool div</div>
+  return <div className="cool-div-3">I'm a cool div</div>
 }
 
 export default CoolDiv
